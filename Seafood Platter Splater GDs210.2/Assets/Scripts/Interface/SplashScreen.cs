35c808db28918e91e0e2363284bb2b8d240e3ae7
Chris-Stoj <1010691@student.sae.edu.au>
2018-02-28 14:17:48 +0800

    Scene setup + Splash screen complete
    Standard water asset uploaded

---
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	public ParticleSystem idleBubble, fastBubble;
+
+	void Start () {
+		//Stop fast bubble effect from playing
+		ParticleSystem.EmissionModule emit = fastBubble.emission;
+		emit.enabled = false;
+	}
+
+	//Press any key
+	void Update () {
+		if (Input.anyKey) {
+			//Stop idle bubble effect from playing
+			ParticleSystem.EmissionModule cease = idleBubble.emission;
+			cease.enabled = false;
+
+			//Play fast bubble effect
+			ParticleSystem.EmissionModule emit = fastBubble.emission;
+			emit.enabled = true;
+			Invoke ("OpenScene", 2f);
+		}
+	}
+
+	//Open next scene in build index order
+	void OpenScene() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
