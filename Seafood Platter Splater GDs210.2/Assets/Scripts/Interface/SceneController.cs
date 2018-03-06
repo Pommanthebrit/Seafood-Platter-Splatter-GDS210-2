b382daf745209c22033944f8d0388bae628f0add
Chris-Stoj <1010691@student.sae.edu.au>
2018-03-06 10:45:45 +0800

    Main Menu Bubbles

---
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	//Open scene button
+	public void OpenScene(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	//Restart current scene
+	public void RestartScene() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	//Quit button
+	public void Quit() {
+		Debug.Log ("Application Closed");
+		Application.Quit();
+	}
+}
