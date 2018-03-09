170b28720b55251dc5ab8d095902c06dc2e3f029
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-03-09 08:02:36 +0800

    Added Looping mechanic to noodle movement

--- Seafood Platter Splater GDs210.2/Assets/Scripts/Enemy/Movement/Automated/EnemyMovementSimpleNoodle.cs
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Enemy/Movement/Automated/EnemyMovementSimpleNoodle.cs
@@ -5,17 +5,101 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyMovementSimpleNoodle : MonoBehaviour 
 {
+	[SerializeField] private bool _loop;
+	[SerializeField] private int _loopAmount;
+	[SerializeField] private float _turnAroundOverscan;
+
 	[SerializeField] private float _horizontalSpeed;
 	[SerializeField] private float _verticalSpeed;
 	[SerializeField] private float _velocityTurnPoint;
+
+	private Renderer _myRenderer;
+	private bool _enteredScreen;
+	private float _currentLoop;
 
 	private bool _rising;
 	private Rigidbody _rb;
 
 	private void Start()
 	{
+		_enteredScreen = false;
+		_myRenderer = GetComponent<Renderer>();
 		_rb = GetComponent<Rigidbody>();
 		_rb.velocity = new Vector3(_horizontalSpeed, 0.0f, 0.0f);
+//		_isReversing = true;
+	}
+
+//	private void Update()
+//	{
+//		if(_myRenderer.isVisible)
+//		{
+//			_isReversing = false;
+//		}
+//		else if(_loop && !_isReversing)
+//		{
+//			if(_currentLoop < _loopAmount && _loopAmount != 0)
+//			{
+//				TurnAround();
+//			}
+//			else if(_loopAmount != 0)
+//			{
+//				// Lose Score.
+//				Destroy(this.gameObject); // Temp.
+//			}
+//
+//			if(_loopAmount == 0)
+//			{
+//				TurnAround();
+//			}
+//		}
+//	}
+
+	private void Update()
+	{
+		Vector3 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+		print(screenPos);
+
+		if(_enteredScreen)
+		{
+			if(screenPos.x > (1.0f + _turnAroundOverscan) || screenPos.x < (0.0f - _turnAroundOverscan))
+			{
+				if(_loop)
+				{
+					if(_currentLoop < _loopAmount && _loopAmount != 0)
+					{
+						TurnAround();
+					}
+					else if(_loopAmount != 0)
+					{
+						// Lose Score.
+						Destroy(this.gameObject); // Temp.
+					}
+					else
+					{
+						TurnAround();
+					}
+				}
+				else
+				{
+					// Lose Score.
+					Destroy(this.gameObject); // Temp.
+				}
+			}
+		}
+		else
+		{
+			if(screenPos.x > 0.0f && screenPos.x < 1.0f)
+			{
+				_enteredScreen = true;
+			}
+		}
+	}
+
+	private void TurnAround()
+	{
+//		_isReversing = true;
+		_rb.velocity = new Vector3(_rb.velocity.x * -1, 0.0f, 0.0f);
+		_currentLoop++;
 	}
 
 	private void FixedUpdate()
