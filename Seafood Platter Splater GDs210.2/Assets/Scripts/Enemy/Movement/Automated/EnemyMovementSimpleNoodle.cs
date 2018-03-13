26ba0906e82fea09e540fb65645e526766b2b663
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-03-13 08:16:36 +0800

    SimpleNoodleMovement Cleanup

--- Seafood Platter Splater GDs210.2/Assets/Scripts/Enemy/Movement/Automated/EnemyMovementSimpleNoodle.cs
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Enemy/Movement/Automated/EnemyMovementSimpleNoodle.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class moves the object in a noodle formation in a simple way.
+// TO-DO_0: Code Segment in Update() method needs revising
+// TO-DO_1: Needs connection to GameGod to lose score (aka fish life)
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyMovementSimpleNoodle : MonoBehaviour 
 {
+	[Header("Loop Settings")]
+
+	[Tooltip("Enables looping")]
 	[SerializeField] private bool _loop;
+
+	[Tooltip("How many times object will loop from A to B. (1 movement (ie. A to B or B to A) is considered 1 loop.")]
 	[SerializeField] private int _loopAmount;
+
+	[Tooltip("How far the object will move off screen before looping (turning around).")]
 	[SerializeField] private float _turnAroundOverscan;
 
+
+	[Header("Speed Settings")]
+
+	[Tooltip("The speed of which the object will move horizontally across the screen.")]
 	[SerializeField] private float _horizontalSpeed;
+
+	[Tooltip("The speed of which the object will move up and down the screen.")]
 	[SerializeField] private float _verticalSpeed;
+
+	[Tooltip("The y-velocity of which the object will reach before reversing the force input.")]
 	[SerializeField] private float _velocityTurnPoint;
 
+	// References.
 	private Renderer _myRenderer;
+	private Rigidbody _rb;
+
+	// Looping Variables.
 	private bool _enteredScreen;
 	private float _currentLoop;
 
+	// Moving Variables.
 	private bool _rising;
-	private Rigidbody _rb;
 
 	private void Start()
 	{
@@ -26,53 +48,31 @@ public class EnemyMovementSimpleNoodle : MonoBehaviour
 		_myRenderer = GetComponent<Renderer>();
 		_rb = GetComponent<Rigidbody>();
 		_rb.velocity = new Vector3(_horizontalSpeed, 0.0f, 0.0f);
-//		_isReversing = true;
 	}
-
-//	private void Update()
-//	{
-//		if(_myRenderer.isVisible)
-//		{
-//			_isReversing = false;
-//		}
-//		else if(_loop && !_isReversing)
-//		{
-//			if(_currentLoop < _loopAmount && _loopAmount != 0)
-//			{
-//				TurnAround();
-//			}
-//			else if(_loopAmount != 0)
-//			{
-//				// Lose Score.
-//				Destroy(this.gameObject); // Temp.
-//			}
-//
-//			if(_loopAmount == 0)
-//			{
-//				TurnAround();
-//			}
-//		}
-//	}
 
 	private void Update()
 	{
+		// Gets screen position relative to this object transform.
 		Vector3 screenPos = Camera.main.WorldToViewportPoint(transform.position);
-		print(screenPos);
 
+		// TO-DO_0: This code needs revising.
 		if(_enteredScreen)
 		{
+			// If object is outside given boundaries.
 			if(screenPos.x > (1.0f + _turnAroundOverscan) || screenPos.x < (0.0f - _turnAroundOverscan))
 			{
 				if(_loop)
 				{
+					// If current loop is less than loop amount and that loop amount is not set to zero (the loop indefinitely value).
 					if(_currentLoop < _loopAmount && _loopAmount != 0)
 					{
 						TurnAround();
 					}
 					else if(_loopAmount != 0)
 					{
-						// Lose Score.
+						// TO-DO_1: Add appropriate fish death by default method.
 						Destroy(this.gameObject); // Temp.
+						// :TO-DO_1.
 					}
 					else
 					{
@@ -81,8 +81,9 @@ public class EnemyMovementSimpleNoodle : MonoBehaviour
 				}
 				else
 				{
-					// Lose Score.
+					// TO-DO_1: Add appropriate fish death by default method.
 					Destroy(this.gameObject); // Temp.
+					// :TO-DO_1.
 				}
 			}
 		}
@@ -93,11 +94,12 @@ public class EnemyMovementSimpleNoodle : MonoBehaviour
 				_enteredScreen = true;
 			}
 		}
+		// :TO-DO_0.
 	}
 
+	// Applies exact opposite x-velocity and increases currentLoop.
 	private void TurnAround()
 	{
-//		_isReversing = true;
 		_rb.velocity = new Vector3(_rb.velocity.x * -1, 0.0f, 0.0f);
 		_currentLoop++;
 	}
@@ -107,20 +109,19 @@ public class EnemyMovementSimpleNoodle : MonoBehaviour
 		Move();
 	}
 
+	// Moves object up and down acording to speed and the set velocity turn point.
 	private void Move()
 	{
+		// If y-velocity is more than the turn point or less than negative value of the turn point, switch _rising state.
 		if(_rb.velocity.y > _velocityTurnPoint || _rb.velocity.y < -_velocityTurnPoint)
 		{
 			if(_rising)
-			{
 				_rising = false;
-			}
 			else
-			{
 				_rising = true;
-			}
 		}
 
+		// Adds the appropriate force to make the object either rise or fall depending on bool.
 		if(_rising)
 		{
 			_rb.AddForce(transform.up * _verticalSpeed, ForceMode.Force);
