f03c41e3a785d4b906ceab43a93383dca7e96769
Chris-Stoj <1010691@student.sae.edu.au>
2018-03-13 14:28:15 +0800

    Menu BtnParticles

--- Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/SceneController.cs
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/SceneController.cs
@@ -4,44 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
-
-	//Open scene button
-	public void OpenScene(string sceneName){
-		SceneManager.LoadScene (sceneName);
+	public GameObject particlesToSpawn;
+	//Open scene button with delay
+	public void OpenSceneDelayed(string sceneName) {
+		StartCoroutine("Load", sceneName);
 	}
 
-	//Restart current scene
-	public void RestartScene() {
-		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	IEnumerator Load(string sceneName) { //coroutine for loading a scene
+		yield return new WaitForSeconds (0.3f); //waits for 1 second
+		SceneManager.LoadScene (sceneName); //Loads the assigned scene when this function is run
 	}
 
-	//Quit button
-	public void Quit() {
-		Debug.Log ("Application Closed");
+	//Quit button with delay
+	public void QuitDelayed(){ //function quit quitting with delay
+		Debug.Log ("Application Closing");
+		Invoke ("Quit", 0.3f); //calls function with specified delay
+	}
+
+	public void Quit() { 
 		Application.Quit();
 	}
 
-	//	public void OpenSceneDelayed(string sceneName){ //function for loading scenes with a delay
-	//		StartCoroutine("Load", sceneName); //Calls coroutine
-	//	}
-	//
-	//	IEnumerator Load(string sceneName) { //coroutine for loading a scene
-	//		yield return new WaitForSeconds (0.3f); //waits for 1 second
-	//		SceneManager.LoadScene (sceneName); //Loads the assigned scene when this function is run
-	//	}
-	//
-	//	public void PausedQuit(string sceneName){ //function for loading main menu while paused
-	//		Time.timeScale = 1f; //sets timescale to 0 (paused)
-	//		SceneManager.LoadScene (sceneName); //Loads the assigned scene when this function is run
-	//	}
-	//
-	//	public void QuitDelayed(){ //function quit quitting with delay
-	//		Invoke ("Quit", 0.3f); //calls function with specified delay
-	//	}
-	//
-	//	public void Quit(){ //function for quitting the game when Quit button is clicked
-	//		print("Quitting game"); //"Displays "Quitting game" to console
-	//		Application.Quit(); //Quits the application (Built game only, doesn't work within Unity editor)
-	//	}
+	public void SpawnParticleEffect(RectTransform rT){
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint (new Vector3(rT.position.x,rT.position.y, 20));
 
+		Instantiate (particlesToSpawn,worldPos, Quaternion.identity);
+	}
 }
