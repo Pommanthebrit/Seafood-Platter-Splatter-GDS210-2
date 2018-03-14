8d42deca91c07723e313bd3a1df9905ba14e9acb
CrimsonFury82 <1007626@student.sae.edu.au>
2018-03-14 15:58:50 +0800

    Aston
    scoreboard tweaks

--- Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/ScoreBoardController.cs
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/ScoreBoardController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; //allows access to UI elements
+using UnityEngine.UI;
 
 //script written by Aston Olsen
 
@@ -19,13 +19,12 @@ public class ScoreBoardController : MonoBehaviour {
 	void Start () {
 		//PlayerPrefs.SetInt ("Score", 20);
 		myScore = PlayerPrefs.GetInt ("Score"); //Gets players current score from player prefs
-		print ("Score " + myScore);
 		finalScore = PlayerPrefs.GetInt ("highScoreValues" + 9); //Gets #10 high score
 			if (myScore > finalScore) { //checks if current score > #10 high score
 				NewHighScore();	//calls function with time delay
 			} else {
-				nameInputButton.SetActive (false);
-				submitButton.SetActive (false);
+				nameInputButton.SetActive (false); //Disables name input box
+				submitButton.SetActive (false); //Disables submit button
 			}
 		highScoreValues = new int[highScores.Length]; //Sets the number of array entries in highScoreValues to the same amount as the array length of highScores
 		highScoreNames = new string[highScores.Length]; //Sets the number of array entries in highScoreNames to the same amount as the array length of highScores
@@ -39,14 +38,7 @@ public class ScoreBoardController : MonoBehaviour {
 		DrawScores ();
 	}
 
-	void SaveScores(){ //function for saving scores
-		for (int x = 0; x < highScores.Length; x++) { //runs this loop for each score in the highScores array
-			PlayerPrefs.SetInt ("highScoreValues" + x, highScoreValues [x]); //each time the loop runs, sets one of the highScoreValues from PlayerPrefs
-			PlayerPrefs.SetString ("highScoreNames" + x, highScoreNames [x]); //each time the loop runs, sets one of the highScoreNames from PlayerPrefs
-		}
-	}
-
-	public void CheckForHighScore(int _value, string _userName){ //function for checking high scores
+	public void UpdateHighScore(int _value, string _userName){ //function for updating high scores
 		for (int x = 0; x < highScores.Length; x++) { //runs this loop for each score in the highScores array
 			if (_value > highScoreValues [x]) { //checks if the users score value is greater than the array value currently being checked from highScoreValues. If it is, do the for loop below
 				for (int y = highScores.Length -1; y > x; y--){ //checks if the new score value is greater than the next score down on the list. If it is, do the loop steps below
@@ -68,14 +60,20 @@ public class ScoreBoardController : MonoBehaviour {
 		} 
 	}
 
+	void SaveScores(){ //function for saving scores
+		for (int x = 0; x < highScores.Length; x++) { //runs this loop for each score in the highScores array
+			PlayerPrefs.SetInt ("highScoreValues" + x, highScoreValues [x]); //each time the loop runs, sets one of the highScoreValues from PlayerPrefs
+			PlayerPrefs.SetString ("highScoreNames" + x, highScoreNames [x]); //each time the loop runs, sets one of the highScoreNames from PlayerPrefs
+		}
+	}
+
 	public void SubmitScore(){ //function for entering name on scoreboard. This function will be called by clicking the "Submit" button on the scoreboard (after the player has entered their name)
-		CheckForHighScore (myScore, playerName.text); //calls function and passes variables
+		UpdateHighScore (myScore, playerName.text); //calls function and passes variables
 		nameInputButton.SetActive (false); //Disables name input box
 		submitButton.SetActive (false); //Disables submit button
 	}
 
 	public void NewHighScore() { //function for when player gets a new high score
-		//We could add highscore UI pop-ups here
 		myAudioSource = GetComponent<AudioSource> (); //Gets audio source
 		myAudioSource.PlayOneShot (wowNewHighScore); //Plays audio file
 	}
