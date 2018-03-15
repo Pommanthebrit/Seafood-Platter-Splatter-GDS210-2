cef787cfe16242c6e2b294660bd3dacb393bbb61
Chris-Stoj <1010691@student.sae.edu.au>
2018-03-15 23:57:20 +0800

    Blood effect fix

---
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Interface/DestroySelf.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+	void Start () {
+		Invoke ("DestroyMe", 1f);
+	}
+
+	void DestroyMe () {
+		Destroy (gameObject);
+	}
+}
