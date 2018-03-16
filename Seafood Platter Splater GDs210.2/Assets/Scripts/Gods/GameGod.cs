a49b35c4aad620d46b88695dc9de28f647d7c4ca
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-03-16 09:19:34 +0800

    Bug Fix on gg. SlowGoldFish added.

--- Seafood Platter Splater GDs210.2/Assets/Scripts/Gods/GameGod.cs
+++ Seafood Platter Splater GDs210.2/Assets/Scripts/Gods/GameGod.cs
@@ -19,8 +19,8 @@ public class GameGod : MonoBehaviour
 
 	//UI Elements
 	[Header("UI Elements")]
-	[SerializeField] private GameObject _ammoHUD;
-	[SerializeField] private GameObject _clipHUD;
+//	[SerializeField] private GameObject _ammoHUD;
+//	[SerializeField] private GameObject _clipHUD;
 	[SerializeField] private GameObject _fishHUD;
 	[SerializeField] private GameObject _ammoHUD_2;
 	[SerializeField] private GameObject _fishHUD_2;
@@ -78,8 +78,8 @@ public class GameGod : MonoBehaviour
 	{
 		//Debug.Log("Total Fish GG: " + _totalFish);
 
-		_ammoHUD.GetComponent<Text> ().text = "x" + _playerControllers[0]._currentAmmo.ToString (); // Player one current ammo
-		_clipHUD.GetComponent<Text> ().text = "x" + _playerControllers[0]._currentClip.ToString (); // Player one current clip
+//		_ammoHUD.GetComponent<Text> ().text = "x" + _playerControllers[0]._currentAmmo.ToString (); // Player one current ammo
+//		_clipHUD.GetComponent<Text> ().text = "x" + _playerControllers[0]._currentClip.ToString (); // Player one current clip
 		_fishHUD.GetComponent<Text> ().text = "x" + _totalFish.ToString ();
 
 		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonUp("Start"))
